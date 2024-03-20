07865473c5a7964d4458a2cea6f45d30bbef54aa
manishashirsath2001 <manishashirsath0204@gmail.com>
2024-03-20 17:25:08 +0530

    pull changes

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, FlatList, Text, TouchableOpacity, ScrollView,ToastAndroid} from 'react-native';
+import { StyleSheet, View,TextInput, FlatList, Text, TouchableOpacity, ScrollView,ToastAndroid} from 'react-native';
 import { ListItem } from 'react-native-elements';
 import axios from 'axios';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Modal from "react-native-modal";
 
 const OpenModal = ({ visible, onClose, onSave }) => {
   const [customerName, setCustomerName] = useState('');
+  const [nameVerify, setNameVerify] =useState('');
   const [mobileNumber, setMobileNumber] = useState('');
+  const [mobileVerify, setMobileVerify] = useState('');
   const [address, setAddress] = useState('');
-
+  const [addVerify, setAddVerify] = useState('');
+ 
 
   const handleSave = () => {
     onSave(customerName, mobileNumber, address);
@@ -20,6 +25,34 @@ const OpenModal = ({ visible, onClose, onSave }) => {
     onClose();
   };
 
+ function handleName(e){
+  const nameVar=e.nativeEvent.text;
+  setCustomerName(nameVar);
+  setNameVerify(false);
+  const nameRegex = /^[A-Za-z]+(\s[A-Za-z]+)?$/;
+    const nameVerify = nameVar.trim().length >= 2 && nameRegex.test(nameVar); // Minimum length of 2 characters and no spaces
+    setNameVerify(nameVerify);
+   
+ 
+ }
+ function handlemobile(e){
+  const mobileVar = e.nativeEvent.text;
+  setMobileNumber(mobileVar);
+  setMobileVerify(false);
+  if(/[7-9]{1}[0-9]{9}/.test(mobileVar)){
+    setMobileNumber(mobileVar);
+    setMobileVerify(true);
+  }
+}
+
+ function handleAddress(e){
+  const addressVar=e.nativeEvent.text;
+  setAddress(addressVar);
+  setAddVerify(false);
+  if(addressVar.length > 1){
+    setAddVerify(true);
+  }
+ }
   return (
     <Modal
       animationType="slide"
@@ -34,27 +67,46 @@ const OpenModal = ({ visible, onClose, onSave }) => {
           </TouchableOpacity>
 
           <Text style={styles.modalText}>Customer Name:</Text>
+          <View style={styles.modelerr}>
           <TextInput
-            style={styles.input}
+           style={styles.input}
             value={customerName}
-            onChangeText={setCustomerName}
+            onChange={e => handleName(e)}
           />
-
+          {customerName.length < 1 ? null : nameVerify ? (
+            <Feather name="check-circle" color="green" size={20}/>
+          ):(
+            <MaterialIcons name="error" color="#C34A2C" size={20}/>
+          )}
+          </View>
           <Text style={styles.modalText}>Customer Mobile Number:</Text>
+          <View style={styles.modelerr}>
           <TextInput
             style={styles.input}
             value={mobileNumber}
-            onChangeText={setMobileNumber}
             keyboardType="numeric"
+            maxLength={10}
+            onChange={e => handlemobile(e)}
           />
-
+          {mobileNumber.length < 1 ? null : mobileVerify ? (
+            <Feather name="check-circle" color="green" size={20}/>
+          ):(
+            <MaterialIcons name="error" color="#C34A2C" size={20}/>
+          )}
+          </View>
           <Text style={styles.modalText}>Address:</Text>
+          <View style={styles.modelerr}>
           <TextInput
             style={styles.input}
             value={address}
-            onChangeText={setAddress}
+            onChange={e => handleAddress(e)}
           />
-
+          {address.length < 1 ? null : addVerify ? (
+            <Feather name="check-circle" color="green" size={20}/>
+          ):(
+            <MaterialIcons name="error" color="#C34A2C" size={20}/>
+          )}
+          </View>
           <TouchableOpacity style={styles.button} onPress={handleSave}>
             <Text style={styles.buttonText}>Save</Text>
           </TouchableOpacity>
@@ -172,8 +224,8 @@ const HomeScreen = ({ navigation }) => {
       console.log('res', response);
 
       if (status === "success") {
-        setSession();
-        ToastAndroid.show(message, ToastAndroid.SHORT);
+       
+        ToastAndroid.show("Register Successfully", ToastAndroid.SHORT);
       } else {
         console.error('Registation Failed:', message);
         ToastAndroid.show(message, ToastAndroid.SHORT);
@@ -289,7 +341,6 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     backgroundColor: 'white',
     borderWidth: 2,
-    marginBottom: 20,
     paddingHorizontal: 10,
   },
   button: {
@@ -297,6 +348,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
+    marginTop:20
   },
   buttonText: {
     color: 'white',
@@ -309,4 +361,11 @@ const styles = StyleSheet.create({
     top: 10,
     right: 10,
   },
+  modelerr:{
+    flexDirection:'row',
+    alignContent:'center',
+    alignItems:'center',
+    justifyContent:'center',
+    alignSelf:'center'
+  }
 });
