c2c683eeea69a339c4b6f3f6a5cd75ab9f997fed
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-08-03 12:40:46 +0530

    pull updated code

--- src/constant/Constants.js
+++ src/constant/Constants.js
@@ -1,10 +1,6 @@
 export const API_URL    = 'https://demo.raviscyber.in/public';  // local
 // export const API_URL    = 'url';  // live
 
-
-
 //Product list
 export const GET_PRODUCT_LIST = "GET_PRODUCT_LIST";
-
-
 export const ADD_TO_BILL = 'ADD_TO_BILL';
